d4050b7c91201fd849231a459221c8693d51a750
Martin Kiss <martin@Martins-MacBook-Pro.local>
2014-06-01 21:23:12 +0100

    adding updated level.JS information
    1) Added extra level to the game
    2) Added extra Alien to the game
    3) Updated sprite card positions
    4) Added comments to code
    5) New sounds added

---
+++ AlienInvaders-master/js/level.js
@@ -0,0 +1,89 @@
+
+  var levelData = { 
+     1:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,3,3,0,0,0,0],
+          [0,0,0,0,0,2,2,0,0,0,0],
+          [0,0,0,0,0,1,1,0,0,0,0]],
+     2:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,3,3,3,3,3,3,3,3,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]],
+     3:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,3,3,3,3,3,3,3,3,0],
+          [0,0,3,3,3,3,3,3,3,3,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]] };
+
+//drawing sprites
+  var spriteData = {
+    'alien1': { sx: 0,  sy: 0,  w: 24, h: 24, cls: Alien },
+    'alien2': { sx: 24,  sy: 0, w: 24, h: 24, cls: Alien },
+    'alien3': { sx: 48,  sy: 0, w: 24, h: 24, cls: Alien },
+    'player': { sx: 0,  sy: 24, w: 24, h: 24, cls: Player },
+    'missile': { sx: 1,  sy: 48, w: 5,  h: 5, cls: Missile }
+  }
+
+//starting the game, being promped to press relevant key.
+  function startGame() {
+    var screen = new GameScreen("Chase the bandits","press space to start",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+    Game.loop();
+  }
+
+//end of the game, prompting us to press relevant key to restart.
+  function endGame() {
+    var screen = new GameScreen("Game Over","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+
+//winning screen, prompting us to press relevent key to restart.
+  function winGame() {
+    var screen = new GameScreen("You Win!","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+//initializing the audio for 
+  $(function() {
+    GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
+                   function() { 
+                       Game.initialize("#gameboard", levelData, spriteData,
+                                      { "start": startGame,
+                                        "die"  : endGame,
+                                        "win"  : winGame });
+                   });
+   });
+
+
+  
+
+
